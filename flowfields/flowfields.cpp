ed922016803f9fc867ddf4ca1cb91cbec52c232a
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-07-06 09:50:41 +0530

    setting-up

--- flowfields/flowfields.cpp
+++ flowfields/flowfields.cpp
@@ -3,6 +3,7 @@
 int main(){
     const int width = 1000;
     const int height = 1000;
+    const float PIE = 3.14;
 
     int left_x = (width * -0.5);
     int right_x = (width * 1.5);
@@ -13,6 +14,14 @@ int main(){
 
     int num_columns = (right_x - left_x) / resolution;
     int num_rows = (bottom_y - top_y) / resolution;
+
+    const float default_angle = PIE * 0.25;
+
+    for (int column = 0; column < num_columns; ++column) {
+        for (int row = 0; row < num_rows; ++row) {
+            grid[column][row] = default_angle;
+        }
+    }
 
     InitWindow(1000, 1000, "First Raylib project"); //Initialize window(width 800, height 800, window name)
     SetTargetFPS(60); // Setting the frame rate. 
