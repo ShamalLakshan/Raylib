04fc7c92c25d6ef6614dadbc7035d111d0e5e76b
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-09 08:15:00 +0530

    init

---
+++ conways-game-of-life/conway.cpp
@@ -0,0 +1,14 @@
+#include <raylib.h>
+
+int main(){
+    const int WINDOW_WIDTH = 750;
+    const int WINDOW_HEIGHT = 750;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
+
+
+    while(WindowShouldClose() == false){
+
+    }
+
+    CloseWindow();
+}
