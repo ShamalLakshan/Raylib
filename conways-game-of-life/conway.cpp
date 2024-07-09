fec63618ada665f7d62bbd525bc39ada0b5aa47d
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-09 08:21:21 +0530

    setup

--- conways-game-of-life/conway.cpp
+++ conways-game-of-life/conway.cpp
@@ -3,11 +3,18 @@
 int main(){
     const int WINDOW_WIDTH = 750;
     const int WINDOW_HEIGHT = 750;
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
+    int FPS = 12;
 
+    Color GREY = {29, 29, 29, 255};
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
+    SetTargetFPS(FPS);
 
     while(WindowShouldClose() == false){
+        BeginDrawing();
+        ClearBackground(GREY);
 
+        EndDrawing();
     }
 
     CloseWindow();
