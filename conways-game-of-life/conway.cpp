7516bc40de5a8dbf3e91997bfd39ee652038eb24
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-09 08:35:00 +0530

    grid

--- conways-game-of-life/conway.cpp
+++ conways-game-of-life/conway.cpp
@@ -4,6 +4,7 @@ int main(){
     const int WINDOW_WIDTH = 750;
     const int WINDOW_HEIGHT = 750;
     int FPS = 12;
+    const int CELL_SIZE = 25;
 
     Color GREY = {29, 29, 29, 255};
 
---
+++ conways-game-of-life/grid.cpp
@@ -0,0 +1,6 @@
+#include "grid.hpp"
+
+void Grid::Draw()
+{
+    for(int row = 0; row < rows; row++)
+}
