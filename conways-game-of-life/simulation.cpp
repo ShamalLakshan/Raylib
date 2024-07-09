9131912d9a2eb28e4858fc78649e3e5b921e37a0
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-09 11:18:39 +0530

    game-logic

--- conways-game-of-life/simulation.cpp
+++ conways-game-of-life/simulation.cpp
@@ -43,7 +43,34 @@ void Simulation::Update()
     {
         for(int column = 0; column < grid.GetColumns(); column++)
         {
+            int liveNeighbors = CountLiveNeighbors(row, column);
+            int cellValue = grid.GetValue(row, column);
+
+            if (cellValue == 1)
+            {
+                if (liveNeighbors > 3 || liveNeighbors < 2)
+                {
+                    tempGrid.SetValue(row, column, 0);
+                }
+                else
+                {
+                    tempGrid.SetValue(row, column, 1);
+                }
+                
+            }
+            else
+            {
+                if (liveNeighbors == 3)
+                {
+                    tempGrid.SetValue(row, column, 1);
+                }
+                else
+                {
+                    tempGrid.SetValue(row, column, 0);
+                }
+            }
             
         }
     }
+    grid = tempGrid;
 }
